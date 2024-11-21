fd0ff1f793c868313a2a9bc3037c16cb9e8993c7
abduvohid-jck <abduvohidmurodov26@gmail.com>
2024-11-22 02:13:51 +0500

    Russian project is completed

---
+++ js/index.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          xl: "1110px",
+        },
+      },
+      fontFamily: {
+        osans: ["osans"],
+        tinos: ["tinos"],
+        cookie: ["cookie"],
+      },
+    },
+  },
+};
